4692b8c82ef3474e0ff41f093e89d1456e367f6c
danilobml <danbarml@gmail.com>
2022-05-07 10:23:38 +0200

    variables

--- src/Flag.js
+++ src/Flag.js
@@ -1,9 +1,14 @@
 const Flag = ({ countryInfo }) => {
   console.log(countryInfo);
   return (
-    <div>
-      <img src={countryInfo[0].flags.png} />
-    </div>
+    <>
+      <div>
+        <h3>{countryInfo[0].name.common}</h3>
+      </div>
+      <div className="flag">
+        <img src={countryInfo[0].flags.png} />
+      </div>
+    </>
   );
 };
 
--- src/MyMap.js
+++ src/MyMap.js
@@ -1,13 +1,7 @@
-import { Map } from 'pigeon-maps'
+import { Map } from "pigeon-maps";
 
-const MyMap = ( {info} ) => {
-  return (
-    <Map 
-      height={300}
-      center={[info.location.lat, info.location.lng]} 
-      zoom={11}  
-    />
-  )
-}
+const MyMap = ({ info }) => {
+  return <Map height={300} center={[info.location.lat, info.location.lng]} zoom={11} className="map" />;
+};
 
 export default MyMap;
